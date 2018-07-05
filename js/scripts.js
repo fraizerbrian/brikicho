403e745e810b9b8170e87f04fcc231c7e76eb6df
fraiser <fraizerkilonzo@gmail.comgit config --user.name fraisergit config --global user.email fraizerkilonzo@gmail.com>
2018-07-05 09:01:19 +0300

    added changes

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#peek-a-boo-showing").fadeToggle();
+    $("#peek-a-boo-hidden").slideToggle();
+  });
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+  $("button#hello").click(function() {
+    $("ul#user").prepend("<li>Hello!</li>");
+    $("ul#webpage").prepend("<li>Why hello there!</li>");
+    $('li').css('background-color', 'green')
+  });
+  $("button#goodbye").click(function() {
+    $("ul#user").prepend("<li>Goodbye!</li>");
+    $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
+  });
+  $("button#stop").click(function() {
+    $("ul#user").prepend("<li>Stop copying me!</li>");
+    $("ul#webpage").prepend("<li>Pardon me. I meant no offense!</li>");
+  });
+});
